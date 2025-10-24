2b6a63cc75d6ac8c2cd5223cf1807a830b156f03
rcn <sigmarust3@gmail.com>
2025-10-24 16:41:19 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'qb-airtag - self explanatory'
+author 'rcnrqvet'
+version '1.0.5'
+
+shared_script '@qb-core/shared/locale.lua'
+
+server_script 'server/main.lua'
+client_script 'client/main.lua'
+
+lua54 'yes'
